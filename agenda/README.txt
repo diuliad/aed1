f2b7ed0390fd49371e4d467ad8dee2a6bc2152c5
Diulia <49985972+diuliad@users.noreply.github.com>
2021-09-26 23:16:50 -0300

    Agenda completa

---
+++ agenda/README.txt
@@ -0,0 +1,30 @@
+﻿Nome: Diulia Justin Deon
+
+Matrícula: 19102170
+
+GitHub: https://github.com/diuliad/aed1
+
+Link dos vídeos: https://drive.google.com/drive/folders/1UUCPgYcsqB4HDNAMK-gOAU_nXt7BtFji?usp=sharing
+
+-- Exercício --
+
+Agenda
+
+Continuar a sua implementação da agenda (exercício 3) semana 1 dentro dos mesmo parâmetros, mas incluir o seguinte.
+Nenhuma variável pode ser declarada em todo o programa, somente ponteiros. Todos os dados do programa devem ser guardados dentro do pBuffer.
+Nem mesmo como parâmetro de função. Só ponteiros que apontam para dentro do pBuffer.
+Exemplo do que não pode: int c; char a; int v[10];  void Funcao(int parametro)
+Não pode usar struct em todo o programa.
+Usar fila ordenada (heap) para armazenar as pessoas em ordem alfabética sempre que o usuário incluir uma nova pessoa.
+Implementar a base de dados da agenda usando lista duplamente ligada
+Somente essa base de dados pode ficar fora do buffer principal, ou seja, pode usar um malloc para cada nodo.
+Seguir as orientações elaborados pelo monitor para código!
+Entregar um vídeo explicando o código e mostrando alguns testes e ao final o resultado do DrMemory.
+
+-- Execução--
+
+O exercício deve ser executados por um compilador C, cada um se encontra dentro da pasta com o respectivo nome. 
+
+-- Avaliação --
+
+Tive bastante dificuldade para desenvolver a Agenda, a semana com chance extra me ajudou bastante, procurei meu colega Juathan, que tinha conseguido fazer e consegui entender e, de certa forma, desenvolver e aprender com o que era previsto. Consegui entender e pensar em coisas que não tinha antes, como os #define no início do código, como falei no vídeo. No mais, além de ter sido tenso, aprendi bastante.
---
+++ agenda/agenda.c
@@ -0,0 +1,242 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Auxiliares
+#define COUNTER (sizeof(int))
+#define OPTION (sizeof(int))
+#define AUX_POINTERPOINTER (sizeof(void **))
+
+//Informações da Pessoa
+#define NAME (sizeof(char) * 10)
+#define AGE (sizeof(int))
+#define NUMBER (sizeof(int))
+
+//Finais da fila
+#define FIRST_PERSON (sizeof(int))
+#define LAST_PERSON (sizeof(int) + sizeof(void **))
+
+//Antes e depois do nome
+#define NEXT_PERSON (sizeof(char) * 10 + sizeof(int) + sizeof(int) + sizeof(void **))
+#define PREVIOUS_PERSON (sizeof(char) * 10 + sizeof(int) + sizeof(int))
+
+void adicionar(void *pBuffer, void *pAux);
+void remover(void *pBuffer, void *linkedList, void *pRun);
+void listar(void *pBuffer, void *pAux);
+void buscar(void *pBuffer, void *pAux, void *pRun);
+void sair(void *pAux);
+
+int main(){
+    void *pAux = NULL;
+    pAux = malloc(COUNTER + AUX_POINTERPOINTER + AUX_POINTERPOINTER);
+
+    *(int *)(pAux) = 0;
+
+    void *pBuffer = NULL;
+    pBuffer = malloc(OPTION + AUX_POINTERPOINTER);
+
+    void *pRun = NULL;
+
+    if (!pBuffer || !pAux){
+        printf("Erro ao alocar memoria.\n");
+        exit(1);
+    }
+
+    do{
+        printf("\n\n--MENU-- \n1 - Adicionar \n2 - Remover \n3 - Listar \n4 - Buscar \n5 - Sair \n");
+        scanf("%d", (int *)(pBuffer));
+        getchar();
+
+        switch (*(int *)(pBuffer)){
+            case 1:
+                adicionar(pBuffer, pAux);
+                break;
+            case 2:
+                remover(pBuffer, pAux, pRun);
+                break;
+            case 3:
+                listar(pBuffer, pAux);
+                break;
+            case 4:
+                buscar(pBuffer, pAux, pRun);
+                break;
+            case 5:
+                sair(pAux);
+                free(pBuffer);
+                free(pAux);
+                exit(0);
+                break;
+            default:
+                printf("Opção inexistente.\n");
+                break;
+        }
+    } while (*(int *)(pBuffer) != 5);
+}
+
+void adicionar(void *pBuffer, void *pAux){
+    int *counter = (int *)(pAux);
+
+    void *pPersonInfo = NULL;
+    pPersonInfo = malloc(NAME + AGE + NUMBER + AUX_POINTERPOINTER + AUX_POINTERPOINTER);
+
+    void *pPersonInfoAux = NULL;
+
+    if (!pPersonInfo){
+        printf("Erro ao alocar memória.\n");
+        exit(1);
+    }
+
+    printf("Digite o nome: ");
+    scanf("%s", (char *)pPersonInfo);
+    getchar();
+
+    printf("Digite a idade: ");
+    scanf("%d", &*(int *)(pPersonInfo + NAME));
+    getchar();
+   
+    printf("Digite o telefone: ");
+    scanf("%d", &*(int *)(pPersonInfo + NAME + AGE));
+    getchar();
+
+    *(void **)(pPersonInfo + NEXT_PERSON) = NULL;   
+    *(void **)(pPersonInfo + PREVIOUS_PERSON) = NULL;
+
+    if (*counter == 0){
+        *(void **)(pAux + LAST_PERSON) = pPersonInfo;
+        *(void **)(pAux + FIRST_PERSON) = pPersonInfo;
+        
+        *counter += 1;
+
+        return;
+    }
+
+    pBuffer = *(void **)(pAux + FIRST_PERSON);
+
+    while(pBuffer != NULL){
+        if(strcmp((char *)pPersonInfo, (char *)pBuffer) < 0){
+            *(void **)(pPersonInfo + PREVIOUS_PERSON) = *(void **)(pBuffer + PREVIOUS_PERSON);
+            *(void **)(pPersonInfo + NEXT_PERSON) = pBuffer;
+
+            if(*counter > 1 && *(void **)(pBuffer + PREVIOUS_PERSON) != NULL){
+                pPersonInfoAux = *(void **)(pBuffer + PREVIOUS_PERSON);
+                *(void **)(pPersonInfoAux + NEXT_PERSON) = pPersonInfo;
+            }
+
+            *(void **)(pBuffer + PREVIOUS_PERSON) = pPersonInfo;
+
+            if(*(void **)(pPersonInfo + PREVIOUS_PERSON) == NULL){
+                *(void **)(pAux + FIRST_PERSON) = pPersonInfo;
+            }
+
+            *counter += 1;
+
+            return;
+        }
+
+        if(*(void **)(pBuffer + NEXT_PERSON) == NULL){
+            pPersonInfoAux = pBuffer;
+        }
+
+        pBuffer = *(void **)(pBuffer + NEXT_PERSON);
+    }
+
+    *(void **)(pPersonInfo + PREVIOUS_PERSON) = pPersonInfoAux;
+    *(void **)(pPersonInfoAux + NEXT_PERSON) = pPersonInfo;
+    *(void **)(pAux + LAST_PERSON) = pPersonInfo;
+
+    *counter += 1;
+
+    return;
+
+}
+
+void remover(void *pBuffer, void *pAux, void *pRun){
+    int *counter = (int *)(pAux);
+
+    if (*counter == 0){
+        printf("A agenda está vazia.\n");
+    }
+    else{ //se nao
+        printf("O primeiro nome da lista foi removido!\n\n");
+        pRun = *(void **)(pAux + FIRST_PERSON);
+        *(void **)(pAux + FIRST_PERSON) = *(void **)(pRun + NEXT_PERSON);
+        free(pRun);
+        *(void **)(pBuffer + PREVIOUS_PERSON) = NULL;
+
+        *counter -= 1;
+
+    }
+}
+
+void listar(void *pBuffer, void *pAux){
+    void *pPersonInfo = NULL;
+    pPersonInfo = *(void **)(pAux + FIRST_PERSON);
+
+    int *counter = (int *)(pAux);
+
+    if(*counter == 0){
+        printf("A agenda está vazia.\n");
+    }
+    else{
+        while(pPersonInfo != NULL){
+            printf("Nome: %s - ", (char *)pPersonInfo);
+            printf("Idade: %d - ", *(int *)(pPersonInfo + NAME));
+            printf("Numero: %d\n\n", *(int *)(pPersonInfo + NAME + AGE));
+
+            pPersonInfo = *(void **)(pPersonInfo + NEXT_PERSON);
+        }
+    }
+}
+
+void buscar(void *pBuffer, void *pAux, void *pRun){
+    int *counter = (int *)(pAux);
+
+    pRun = *(void **)(pAux + FIRST_PERSON);
+
+    if(*counter == 0){
+        printf("Lista vazia.\n");
+        return;
+    } 
+    else{
+        void *auxSearch = malloc(sizeof(char) * 10);
+
+        printf("Digite o nome que queira buscar: \n");
+        scanf("%s", (char *)auxSearch);
+        getchar();
+
+        while(pRun != NULL){
+            if(strcmp(pRun ,(char *)auxSearch) == 0){
+                printf("Nome: %s\n", (char *)pRun);
+                printf("Idade: %d\n", *(int *)(pRun + NAME));
+                printf("Numero: %d\n\n", *(int *)(pRun + NAME + AGE));
+
+                free(auxSearch);
+                
+                return;
+            }
+            
+            pRun = *(void **)(pRun + NEXT_PERSON);
+        }
+    }
+
+    return;
+
+}
+
+void sair(void *pAux){
+    void *pFree = *(void **)(pAux + FIRST_PERSON);
+    int *counter = (int *)(pAux);
+
+    if ((int *)(pAux) == 0){
+        printf("A lista está vazia.");
+    }
+    else{
+        while (*(void **)(pAux + FIRST_PERSON) != NULL) {
+            pFree = *(void **)(pAux + FIRST_PERSON);
+            *(void **)(pAux + FIRST_PERSON) = *(void **)(pFree + NEXT_PERSON);
+            free(pFree);
+
+            *counter -= 1;
+        }
+    }
+}
